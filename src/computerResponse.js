5329c8daadd9f2ff474138255f43501b5862bb52
stasinski <01dawids@gmail.com>
2021-03-15 12:24:33 +0100

    adding forgotten condition

--- src/computerResponse.js
+++ src/computerResponse.js
@@ -15,15 +15,24 @@ export const response = (grid) => {
   // win in one
   for (let i = 0; i < 3; i++) {
     // // row
-    if (grid[i * 3].taken && grid[i * 3].taken === grid[i * 3 + 1].taken) {
+    if (
+      !grid[i * 3 + 2].taken &&
+      grid[i * 3].taken &&
+      grid[i * 3].taken === grid[i * 3 + 1].taken
+    ) {
       const win = grid[i * 3].taken === "COMPUTER";
       bestMove = chooseBetterMove(bestMove, win ? 100 : 80, i * 3 + 2);
     }
-    if (grid[i * 3].taken && grid[i * 3].taken === grid[i * 3 + 2].taken) {
+    if (
+      !grid[i * 3 + 1].taken &&
+      grid[i * 3].taken &&
+      grid[i * 3].taken === grid[i * 3 + 2].taken
+    ) {
       const win = grid[i * 3].taken === "COMPUTER";
       bestMove = chooseBetterMove(bestMove, win ? 100 : 80, i * 3 + 1);
     }
     if (
+      !grid[i * 3].taken &&
       grid[i * 3 + 1].taken &&
       grid[i * 3 + 1].taken === grid[i * 3 + 2].taken
     ) {
@@ -31,40 +40,52 @@ export const response = (grid) => {
       bestMove = chooseBetterMove(bestMove, win ? 100 : 80, i * 3);
     }
     // col
-    if (grid[i].taken && grid[i].taken === grid[i + 3].taken) {
+    if (
+      !grid[i + 6].taken &&
+      grid[i].taken &&
+      grid[i].taken === grid[i + 3].taken
+    ) {
       const win = grid[i].taken === "COMPUTER";
       bestMove = chooseBetterMove(bestMove, win ? 100 : 80, i + 6);
     }
-    if (grid[i].taken && grid[i].taken === grid[i + 6].taken) {
+    if (
+      !grid[i + 3].taken &&
+      grid[i].taken &&
+      grid[i].taken === grid[i + 6].taken
+    ) {
       const win = grid[i].taken === "COMPUTER";
       bestMove = chooseBetterMove(bestMove, win ? 100 : 80, i + 3);
     }
-    if (grid[i + 3].taken && grid[i + 3].taken === grid[i + 6].taken) {
+    if (
+      !grid[i].taken &&
+      grid[i + 3].taken &&
+      grid[i + 3].taken === grid[i + 6].taken
+    ) {
       const win = grid[i + 3].taken === "COMPUTER";
       bestMove = chooseBetterMove(bestMove, win ? 100 : 80, i);
     }
   }
-  if (grid[0].taken && grid[0].taken === grid[4].taken) {
+  if (!grid[8].taken && grid[0].taken && grid[0].taken === grid[4].taken) {
     const win = grid[0].taken === "COMPUTER";
     bestMove = chooseBetterMove(bestMove, win ? 100 : 80, 8);
   }
-  if (grid[0].taken && grid[0].taken === grid[8].taken) {
+  if (!grid[4].taken && grid[0].taken && grid[0].taken === grid[8].taken) {
     const win = grid[0].taken === "COMPUTER";
     bestMove = chooseBetterMove(bestMove, win ? 100 : 80, 4);
   }
-  if (grid[4].taken && grid[4].taken === grid[8].taken) {
+  if (!grid[0].taken && grid[4].taken && grid[4].taken === grid[8].taken) {
     const win = grid[4].taken === "COMPUTER";
     bestMove = chooseBetterMove(bestMove, win ? 100 : 80, 0);
   }
-  if (grid[2].taken && grid[2].taken === grid[4].taken) {
+  if (!grid[6].taken && grid[2].taken && grid[2].taken === grid[4].taken) {
     const win = grid[2].taken === "COMPUTER";
     bestMove = chooseBetterMove(bestMove, win ? 100 : 80, 6);
   }
-  if (grid[2].taken && grid[2].taken === grid[6].taken) {
+  if (!grid[4].taken && grid[2].taken && grid[2].taken === grid[6].taken) {
     const win = grid[2].taken === "COMPUTER";
     bestMove = chooseBetterMove(bestMove, win ? 100 : 80, 4);
   }
-  if (grid[4].taken && grid[4].taken === grid[6].taken) {
+  if (!grid[2].taken && grid[4].taken && grid[4].taken === grid[6].taken) {
     const win = grid[4].taken === "COMPUTER";
     bestMove = chooseBetterMove(bestMove, win ? 100 : 80, 2);
   }
