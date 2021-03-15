21319ca6492409c463d57630fab07890d4281480
stasinski <01dawids@gmail.com>
2021-03-15 08:06:36 +0100

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+### MINMAX algorithm
+
+
+- step 1 UI ✅
+- step 2 Computer random response
+- step 3 First calculation
+- step 4 working algorithm
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/app.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    // filename: "main.[contenthash].js",
+    filename: "main.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      //   filename: "[name].[contenthash].css",
+      filename: "[name].css",
+    }),
+    new CssMinimizerPlugin(),
+  ],
+};
