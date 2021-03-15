11c9dcb19c19604ab139268c6ad1013487b990d9
stasinski <01dawids@gmail.com>
2021-03-15 12:51:32 +0100

    step 4

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 - step 1 UI ✅
 - step 2 Computer random response ✅
 - step 3 First calculation ✅
-- step 4 working algorithm
+- step 4 working algorithm  ✅
--- src/computerResponse.js
+++ src/computerResponse.js
@@ -65,6 +65,7 @@ export const response = (grid) => {
       bestMove = chooseBetterMove(bestMove, win ? 100 : 80, i);
     }
   }
+  // diagonal
   if (!grid[8].taken && grid[0].taken && grid[0].taken === grid[4].taken) {
     const win = grid[0].taken === "COMPUTER";
     bestMove = chooseBetterMove(bestMove, win ? 100 : 80, 8);
@@ -92,11 +93,44 @@ export const response = (grid) => {
 
   for (let move of filteredGrid) {
     if (move.cell === 5) {
-      bestMove = chooseBetterMove(bestMove, 10, move.cell - 1);
+      bestMove = chooseBetterMove(bestMove, 50, move.cell - 1);
+    }
+    if (
+      move.cell === 1 ||
+      move.cell === 3 ||
+      move.cell === 7 ||
+      move.cell === 9
+    ) {
+      let bonus = 0;
+      const row = getRow(move.cell);
+      if (
+        grid[row * 3].taken === "COMPUTER" ||
+        grid[row * 3 + 1].taken === "COMPUTER" ||
+        grid[row * 3 + 2].taken === "COMPUTER"
+      ) {
+        bonus += 13.1;
+      }
+      if (
+        !grid[row * 3].taken &&
+        !grid[row * 3 + 1].taken &&
+        !grid[row * 3 + 2].taken
+      ) {
+        bonus += 4.1;
+      }
+      if (
+        grid[row * 3].taken === "PLAYER" ||
+        grid[row * 3 + 1].taken === "PLAYER" ||
+        grid[row * 3 + 2].taken === "PLAYER"
+      ) {
+        bonus += 8.1;
+      }
+      bestMove = chooseBetterMove(
+        bestMove,
+        Math.random() * 3 + 5 + bonus,
+        move.cell - 1
+      );
     }
   }
-
-  console.log(bestMove);
 
   if (!bestMove) {
     const randomPick = Math.ceil(Math.random() * filteredGrid.length);
@@ -104,5 +138,15 @@ export const response = (grid) => {
     return filteredGrid[randomPick - 1].cell;
   } else {
     return grid[bestMove.id].cell;
+  }
+};
+
+const getRow = (num) => {
+  if (num < 4) {
+    return 0;
+  } else if (num < 7) {
+    return 1;
+  } else {
+    return 2;
   }
 };
